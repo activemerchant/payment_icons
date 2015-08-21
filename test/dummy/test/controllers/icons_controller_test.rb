711560cb6ba4d29756ae8aa644abca639deba8fa
Nakul Pathak <nakul.pathak@shopify.com>
2015-08-21 17:59:21 +0000

    Small correction to icons controller test

--- test/dummy/test/controllers/icons_controller_test.rb
+++ test/dummy/test/controllers/icons_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class IconsControllerTest < ActionController::TestCase
 
-  test "app can access payment_icons" do
+  test "app can access payment_icons' methods" do
     assert_equal ["visa", "master", "discover", "american-express", "diners-club", "jcb"], PaymentIcon.credit_cards.pluck(:name)
   end
 
