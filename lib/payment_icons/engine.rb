f82bcf4ad789b8790503858ffcb8a81636df4317
Jonathan Smith <jonathan.smith@shopify.com>
2015-11-13 15:01:20 -0500

    Precompile assets seperately

--- lib/payment_icons/engine.rb
+++ lib/payment_icons/engine.rb
@@ -1,5 +1,9 @@
 module PaymentIcons
   class Engine < ::Rails::Engine
     isolate_namespace PaymentIcons
+
+    initializer "payment_icons.assets.precompile" do |app|
+      app.config.assets.precompile += %w(*.svg)
+    end
   end
 end
