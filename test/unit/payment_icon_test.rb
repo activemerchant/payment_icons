eff906190f32af843444242277491c49e01560a5
Maxime Vaillancourt <maxime.vaillancourt@shopify.com>
2018-06-01 11:46:03 -0400

    Add role='img' and <title> tag to SVGs

--- test/unit/payment_icon_test.rb
+++ test/unit/payment_icon_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 class PaymentIconTest < ActiveSupport::TestCase
 
+  ICONS_DIRECTORY = PaymentIcons::Engine.root.join("app/assets/images/payment_icons")
+
   test "Every payment icon record is valid" do
     PaymentIcon.all.each do |icon|
       assert_predicate icon.name, :present?
@@ -13,5 +15,41 @@ class PaymentIconTest < ActiveSupport::TestCase
   test '#path returns path within app/assets/images' do
     visa = PaymentIcon.where(name: 'visa').first
     assert_equal 'payment_icons/visa.svg', visa.path
+  end
+
+  test 'Every payment icon record has a corresponding SVG file' do
+    PaymentIcon.all.each do |icon|
+      assert File.exist?(ICONS_DIRECTORY.join("#{icon.name}.svg"))
+    end
+  end
+
+  test 'Every payment icon SVG has a valid XML tree' do
+    PaymentIcon.all.each do |icon|
+      file = File.read(ICONS_DIRECTORY.join("#{icon.name}.svg")).freeze
+      document = Nokogiri::XML.parse(file)
+      assert document.errors.blank?, message: "The '#{icon.name}' SVG file is invalid: #{document.errors}"
+    end
+  end
+
+  test "Every payment SVG meets accessibility requirements" do
+    PaymentIcon.all.each do |icon|
+      file = File.read(ICONS_DIRECTORY.join("#{icon.name}.svg")).freeze
+      document = Nokogiri::XML.parse(file)
+
+      assert_equal 1, document.root.search('title').count,
+        message: "The '#{icon.name}' SVG file should have a single <title> node"
+
+      assert document.root.search('title').present?,
+        message: "The '#{icon.name}' SVG file should have a <title> node as a child of the root <svg> node"
+      
+      assert_equal icon.label, document.root.at('title').content,
+        message: "The '#{icon.name}' SVG file does not have the appropriate <title> value"
+
+      assert document.root.key?('role'),
+        message: "The '#{icon.name}' SVG file should have a 'role' attribute on the root <svg> node"
+        
+      assert_equal "img", document.root['role'],
+        message: "The '#{icon.name}' SVG file should have a role='img' attribute on the root <svg> node"
+    end
   end
 end
