0c281e56ffdbee49dfbfbe8d596070b8a708a801
Maxime Vaillancourt <maxime.vaillancourt@shopify.com>
2018-06-01 16:17:56 -0400

    add svg dimensions test and refactor tests

--- test/unit/payment_icon_test.rb
+++ test/unit/payment_icon_test.rb
@@ -3,6 +3,9 @@ require 'test_helper'
 class PaymentIconTest < ActiveSupport::TestCase
 
   ICONS_DIRECTORY = PaymentIcons::Engine.root.join("app/assets/images/payment_icons")
+  SVG_PAYMENT_TYPES = Dir[ICONS_DIRECTORY.join('*.svg')].each_with_object({}) do |path, hash|
+    hash[File.basename(path, '.svg')] = File.read(path).freeze
+  end.freeze
 
   test "Every payment icon record is valid" do
     PaymentIcon.all.each do |icon|
@@ -19,38 +22,49 @@ class PaymentIconTest < ActiveSupport::TestCase
 
   test 'Every payment icon record has a corresponding SVG file' do
     PaymentIcon.all.each do |icon|
-      assert File.exist?(ICONS_DIRECTORY.join("#{icon.name}.svg"))
+      assert SVG_PAYMENT_TYPES.key?(icon.name)
     end
   end
 
   test 'Every payment icon SVG has a valid XML tree' do
     PaymentIcon.all.each do |icon|
-      file = File.read(ICONS_DIRECTORY.join("#{icon.name}.svg")).freeze
-      document = Nokogiri::XML.parse(file)
+      document = Nokogiri::XML.parse(SVG_PAYMENT_TYPES[icon.name])
       assert document.errors.blank?,
         message: "The '#{icon.name}' SVG file is invalid: #{document.errors}. Please fix the errors, then optimize the file using SVGO."
     end
   end
 
+  test 'Every payment icon SVG is 38x24 pixels' do
+    PaymentIcon.all.each do |icon|
+      document = Nokogiri::XML.parse(SVG_PAYMENT_TYPES[icon.name])
+      assert_equal 38, document.root["width"].to_i,
+        message: "The '#{icon.name}' SVG file must be 38 pixels wide"
+      assert_equal 24, document.root["height"].to_i,
+        message: "The '#{icon.name}' SVG file must be 24 pixels high"
+    end
+  end
+
   test "Every payment SVG meets accessibility requirements" do
     PaymentIcon.all.each do |icon|
-      file = File.read(ICONS_DIRECTORY.join("#{icon.name}.svg")).freeze
-      document = Nokogiri::XML.parse(file)
+      document = Nokogiri::XML.parse(SVG_PAYMENT_TYPES[icon.name])
 
       assert_equal 1, document.root.search('title').count,
-        message: "The '#{icon.name}' SVG file should have a single <title> node"
+        message: "The '#{icon.name}' SVG file should have a single <title> tag"
 
       assert document.root.search('title').present?,
-        message: "The '#{icon.name}' SVG file should have a <title> node as a child of the root <svg> node"
+        message: "The '#{icon.name}' SVG file should have a <title> tag as a child of the root <svg> tag"
+
+      assert_equal "title", document.root.first_element_child.name,
+        message: "The '#{icon.name}' SVG file should have a <title> tag as first child of the root <svg> tag"
       
       assert_equal icon.label, document.root.at('title').content,
         message: "The '#{icon.name}' SVG file does not have the appropriate <title> value"
 
       assert document.root.key?('role'),
-        message: "The '#{icon.name}' SVG file should have a 'role' attribute on the root <svg> node"
+        message: "The '#{icon.name}' SVG file should have a 'role' attribute on the root <svg> tag"
         
       assert_equal "img", document.root['role'],
-        message: "The '#{icon.name}' SVG file should have a role='img' attribute on the root <svg> node"
+        message: "The '#{icon.name}' SVG file should have a role='img' attribute on the root <svg> tag"
     end
   end
 end
