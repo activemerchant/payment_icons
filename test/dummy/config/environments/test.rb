327e3214a11eed5b1f4ba2a370b8a44a441f59f3
Thomas Tau <tau.thomas01@gmail.com>
2020-07-28 08:46:59 -0400

    Fix CI build

--- test/dummy/config/environments/test.rb
+++ test/dummy/config/environments/test.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   config.eager_load = false
 
   config.serve_static_files   = true
-  config.static_cache_control = 'public, max-age=3600'
+  config.public_file_server.headers = { 'Cache-Control' => 'public, max-age=3600' }
   config.assets.digest = true
 
   config.consider_all_requests_local       = true
