43ca5088ad4c4a27dab544d31d0cd244907696b4
Richard Ramsden <richard@rramsden.ca>
2020-08-04 16:33:30 +0900

    Make the tests pass

--- test/dummy/config/environments/test.rb
+++ test/dummy/config/environments/test.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
 
   config.eager_load = false
 
-  config.serve_static_files   = true
+  config.public_file_server.enabled = true
   config.public_file_server.headers = { 'Cache-Control' => 'public, max-age=3600' }
   config.assets.digest = true
 
