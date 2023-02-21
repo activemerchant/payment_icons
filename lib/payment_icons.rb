0a93d31fb6d6b303880ee427cf1a493e860f2df8
Luna Tao <luna.tao@shopify.com>
2023-02-21 14:44:17 -0500

    no rails dependency on gem; need to Fetching gem metadata from https://rubygems.org/........... Resolving dependencies... Using rake 13.0.6 Using concurrent-ruby 1.2.0 Using mini_portile2 2.8.1 Using racc 1.6.2 Using minitest 5.17.0 Using erubi 1.12.0 Using builder 3.2.4 Using crass 1.0.6 Using rack 2.2.6.2 Using nio4r 2.5.8 Using websocket-extensions 0.1.5 Using marcel 1.0.2 Using date 3.3.3 Using mini_mime 1.1.2 Using bundler 2.3.11 Using timeout 0.3.2 Using method_source 1.0.0 Using coderay 1.1.3 Using bump 0.10.0 Using payment_icons 1.7.22 from source at `.` Using thor 1.2.1 Using i18n 1.12.0 Using zeitwerk 2.6.7 Using tzinfo 2.0.6 Using pry 0.14.2 Using rack-test 2.0.2 Using net-protocol 0.2.1 Using nokogiri 1.14.2 (arm64-darwin) Using activesupport 7.0.4.2 Using globalid 1.1.0 Using net-pop 0.1.2 Using websocket-driver 0.7.5 Using activemodel 7.0.4.2 Using net-imap 0.3.4 Using net-smtp 0.3.3 Using loofah 2.19.1 Using rails-dom-testing 2.0.3 Using activejob 7.0.4.2 Using rails-html-sanitizer 1.5.0 Using activerecord 7.0.4.2 Using mail 2.8.1 Using actionview 7.0.4.2 Using actionpack 7.0.4.2 Using actioncable 7.0.4.2 Using railties 7.0.4.2 Using actionmailer 7.0.4.2 Using activestorage 7.0.4.2 Using actionmailbox 7.0.4.2 Using actiontext 7.0.4.2 Using rails 7.0.4.2 Bundle updated! to update Gemfile.lock

--- lib/payment_icons.rb
+++ lib/payment_icons.rb
@@ -1,6 +1,10 @@
-require 'payment_icons/engine'
-require 'frozen_record'
-require 'sass_functions/payment_icons'
-
 module PaymentIcons
+  extend self
+
+  def svgs
+    directory = Pathname.new(File.join(File.dirname(File.expand_path(__FILE__)), '../app/assets/images/payment_icons/'))
+    Dir[directory.join('*.svg')].map do |path|
+      [File.basename(path, '.svg'), File.read(path).freeze]
+    end.to_h.freeze
+  end
 end
